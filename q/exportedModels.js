c0cc27ff8eff3e9ae5a348e4e89807c6033c92b6
Rob de la Vega <delavega.rob@gmail.com>
2025-08-10 16:43:14 -0400

    Add files via upload

---
+++ q/exportedModels.js
@@ -0,0 +1,8 @@
+var JS_Array_Entries_Of_Models = new Array('m4dbef1a0-7243-11f0-24ea-5cb47e4f1a87','m3fde59a0-7134-11f0-24ea-5cb47e4f1a87','m5bdf7e90-713e-11f0-24ea-5cb47e4f1a87','md89290a0-6f0c-11f0-24ea-5cb47e4f1a87','m88654650-723f-11f0-24ea-5cb47e4f1a87','m687e2f20-7139-11f0-24ea-5cb47e4f1a87','m00c89a00-7242-11f0-24ea-5cb47e4f1a87','m1119f760-7142-11f0-24ea-5cb47e4f1a87','m288be420-7134-11f0-24ea-5cb47e4f1a87','ma1a359b0-7143-11f0-24ea-5cb47e4f1a87');function searchModel(model) {
+for (var i = 0; i < JS_Array_Entries_Of_Models.length; i++) {
+if (model == JS_Array_Entries_Of_Models[i]) {
+return true;
+}
+}
+return false;
+}
