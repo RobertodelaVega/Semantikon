22241c30b7bd7db4dee2e95b338dc9e744a59261
Rob de la Vega <delavega.rob@gmail.com>
2025-08-12 14:09:51 -0400

    Add files via upload

---
+++ ComEx/exportedModels.js
@@ -0,0 +1,8 @@
+var JS_Array_Entries_Of_Models = new Array('m4dbef1a0-7243-11f0-24ea-5cb47e4f1a87','m3fde59a0-7134-11f0-24ea-5cb47e4f1a87','m8c4525e0-77a0-11f0-24ea-5cb47e4f1a87','m88654650-723f-11f0-24ea-5cb47e4f1a87','m00c89a00-7242-11f0-24ea-5cb47e4f1a87');function searchModel(model) {
+for (var i = 0; i < JS_Array_Entries_Of_Models.length; i++) {
+if (model == JS_Array_Entries_Of_Models[i]) {
+return true;
+}
+}
+return false;
+}
---
+++ ComEx/tree_data.js
@@ -0,0 +1,9 @@
+var nod_root = new TNode(' ', ' ', '', TROOT);
+//groupf28f9a60-7246-11f0-24ea-5cb47e4f1a87
+var nod_f28f9a60_7246_11f0_24ea_5cb47e4f1a87 = nod_root.TaddChild('ComEx', ' ', '', TFOLDER);
+var nod_e01e0330_7606_11f0_24ea_5cb47e4f1a87 = nod_f28f9a60_7246_11f0_24ea_5cb47e4f1a87.TaddChild('procesos', ' ', '', TFOLDER);
+var nod_4dbef1a0_7243_11f0_24ea_5cb47e4f1a87 = nod_e01e0330_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Arbitraje comercial', 'm4dbef1a0-7243-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_88654650_723f_11f0_24ea_5cb47e4f1a87 = nod_e01e0330_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Inteligencia de  mercado', 'm88654650-723f-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_00c89a00_7242_11f0_24ea_5cb47e4f1a87 = nod_e01e0330_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Negociación internacional', 'm00c89a00-7242-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_3fde59a0_7134_11f0_24ea_5cb47e4f1a87 = nod_f28f9a60_7246_11f0_24ea_5cb47e4f1a87.TaddChild('Comercio Exterior', 'm3fde59a0-7134-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_8c4525e0_77a0_11f0_24ea_5cb47e4f1a87 = nod_f28f9a60_7246_11f0_24ea_5cb47e4f1a87.TaddChild('Empresa ComEx', 'm8c4525e0-77a0-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
