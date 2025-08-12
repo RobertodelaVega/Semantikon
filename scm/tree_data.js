dbfb2266bcddedd46ca731486d3c9fb30dfbdfb7
Rob de la Vega <delavega.rob@gmail.com>
2025-08-12 14:11:45 -0400

    Add files via upload

---
+++ scm/tree_data.js
@@ -0,0 +1,8 @@
+var nod_root = new TNode(' ', ' ', '', TROOT);
+//groupe5dd05f0-7246-11f0-24ea-5cb47e4f1a87
+var nod_e5dd05f0_7246_11f0_24ea_5cb47e4f1a87 = nod_root.TaddChild('SCM', ' ', '', TFOLDER);
+var nod_f4af83f0_7606_11f0_24ea_5cb47e4f1a87 = nod_e5dd05f0_7246_11f0_24ea_5cb47e4f1a87.TaddChild('procesos', ' ', '', TFOLDER);
+var nod_5bdf7e90_713e_11f0_24ea_5cb47e4f1a87 = nod_f4af83f0_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Diseño estratégico de red', 'm5bdf7e90-713e-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_687e2f20_7139_11f0_24ea_5cb47e4f1a87 = nod_f4af83f0_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Logística inversa', 'm687e2f20-7139-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_ea59ed00_77a0_11f0_24ea_5cb47e4f1a87 = nod_e5dd05f0_7246_11f0_24ea_5cb47e4f1a87.TaddChild('SCM Chile', 'mea59ed00-77a0-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_288be420_7134_11f0_24ea_5cb47e4f1a87 = nod_e5dd05f0_7246_11f0_24ea_5cb47e4f1a87.TaddChild('Supply Chain Management', 'm288be420-7134-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
