4dc1ef4a156fe9df19510031fe7cf62b9cef68b2
Rob de la Vega <delavega.rob@gmail.com>
2025-08-12 14:04:56 -0400

    Add files via upload

---
+++ Log/tree_data.js
@@ -0,0 +1,8 @@
+var nod_root = new TNode(' ', ' ', '', TROOT);
+//groupeb3bfe20-7246-11f0-24ea-5cb47e4f1a87
+var nod_eb3bfe20_7246_11f0_24ea_5cb47e4f1a87 = nod_root.TaddChild('Logística', ' ', '', TFOLDER);
+var nod_ed407070_7606_11f0_24ea_5cb47e4f1a87 = nod_eb3bfe20_7246_11f0_24ea_5cb47e4f1a87.TaddChild('procesos', ' ', '', TFOLDER);
+var nod_1119f760_7142_11f0_24ea_5cb47e4f1a87 = nod_ed407070_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Previsión de la demanda', 'm1119f760-7142-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_a1a359b0_7143_11f0_24ea_5cb47e4f1a87 = nod_ed407070_7606_11f0_24ea_5cb47e4f1a87.TaddChild('Transporte Inteligente', 'ma1a359b0-7143-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_d89290a0_6f0c_11f0_24ea_5cb47e4f1a87 = nod_eb3bfe20_7246_11f0_24ea_5cb47e4f1a87.TaddChild('Gestión Logística', 'md89290a0-6f0c-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
+var nod_c837d3e0_77a0_11f0_24ea_5cb47e4f1a87 = nod_eb3bfe20_7246_11f0_24ea_5cb47e4f1a87.TaddChild('Salmones P.Montt', 'mc837d3e0-77a0-11f0-24ea-5cb47e4f1a87_nav.htm', 'model', TMODEL);
