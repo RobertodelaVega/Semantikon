5e9c1f30e862105ab5fbf891b84db15926f0206a
Rob de la Vega <delavega.rob@gmail.com>
2025-08-12 14:12:52 -0400

    Add files via upload

---
+++ scm/exportedModels.js
@@ -0,0 +1,8 @@
+var JS_Array_Entries_Of_Models = new Array('m5bdf7e90-713e-11f0-24ea-5cb47e4f1a87','m687e2f20-7139-11f0-24ea-5cb47e4f1a87','mea59ed00-77a0-11f0-24ea-5cb47e4f1a87','m288be420-7134-11f0-24ea-5cb47e4f1a87');function searchModel(model) {
+for (var i = 0; i < JS_Array_Entries_Of_Models.length; i++) {
+if (model == JS_Array_Entries_Of_Models[i]) {
+return true;
+}
+}
+return false;
+}
